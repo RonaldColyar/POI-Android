49e132fc18a46ed75fd3841391a7dac978c876b2
RonaldColyar <PhantexCreations@gmail.com>
2021-02-16 06:33:56 -0600

    implemented basic Entry U.i layout

---
+++ Android-Poi/app/src/main/java/com/example/myapplication/new_person_form.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class new_person_form : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_person_form)
+    }
+}
