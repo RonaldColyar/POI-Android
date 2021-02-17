c5610431a717cf51fb06fc8803729b8ee851bf1c
RonaldColyar <PhantexCreations@gmail.com>
2021-02-17 05:16:18 -0600

    New dialog implementation

--- Android-Poi/app/src/main/res/layout/EntryAdapter.kt
+++ Android-Poi/app/src/main/res/layout/EntryAdapter.kt
@@ -1,14 +1,35 @@
 package layout
 
+import android.app.Dialog
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import kotlinx.android.synthetic.main.entryviewchild.view.*
 
-class EntryAdapter(var entries :MutableList<Entry>) : RecyclerView.Adapter<ViewHolder>() {
-    
+class EntryAdapter(var entries :MutableList<Entry> , val context:Context) : RecyclerView.Adapter<ViewHolder>() {
+    private fun showDialog(data:Entry) {
+        val dialog = Dialog(context)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.detailedentryview)
+
+        var datelabel = dialog.findViewById(R.id.detailedDate) as TextView
+        var entrylabel = dialog.findViewById(R.id.detailedLabel) as TextView
+        var levellabel = dialog.findViewById(R.id.detailedLabel) as TextView
+        //modify the values to fit entry data before showing
+
+        dialog.show()
+
+    }
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.itemView.EntryChildLabel.text = entries[position].label
+        holder.itemView.EntryChildDate.text = entries[position].date_string
+        holder.itemView.EntryChildLevel.text = entries[position].level
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
